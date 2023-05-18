0726b6c8a32aea35eb9a11c21b1e7ff28b92b6ae
Ciuve <126168193+Ciuve@users.noreply.github.com>
2023-05-18 15:05:19 +0200

    Lavoro

--- src/main/java/it/ciuverca/agenziaviaggi/services/ViaggioService.java
+++ src/main/java/it/ciuverca/agenziaviaggi/services/ViaggioService.java
@@ -1,5 +1,8 @@
 package it.ciuverca.agenziaviaggi.services;
 
+import it.ciuverca.agenziaviaggi.Entities.Cliente;
+import it.ciuverca.agenziaviaggi.Entities.Prenotazioni;
+import it.ciuverca.agenziaviaggi.Entities.Utente;
 import it.ciuverca.agenziaviaggi.Entities.Viaggio;
 
 import java.util.List;
@@ -8,6 +11,13 @@ public interface ViaggioService {
     List<Viaggio> getViaggio();
     Viaggio getViaggioById(int id);
     Viaggio addViaggio(Viaggio v);
-
-
+    List<Utente> getUtente();
+    Utente getUtenteById(int id);
+    Utente addUtente(Utente u);
+    List<Prenotazioni> getPrenotazioni();
+    Prenotazioni getPrenotazioniById(int id);
+    Prenotazioni addPrenotazioni(Prenotazioni prenotazioni);
+    List<Cliente> getCliente();
+    Cliente getClienteById(int id);
+    Cliente addCliente(Cliente cliente);
 }
--- src/main/java/it/ciuverca/agenziaviaggi/services/ViaggioServiceImpl.java
+++ src/main/java/it/ciuverca/agenziaviaggi/services/ViaggioServiceImpl.java
@@ -1,6 +1,12 @@
 package it.ciuverca.agenziaviaggi.services;
 
+import it.ciuverca.agenziaviaggi.Entities.Cliente;
+import it.ciuverca.agenziaviaggi.Entities.Prenotazioni;
+import it.ciuverca.agenziaviaggi.Entities.Utente;
 import it.ciuverca.agenziaviaggi.Entities.Viaggio;
+import it.ciuverca.agenziaviaggi.repos.ClienteDao;
+import it.ciuverca.agenziaviaggi.repos.PrenotazioniDao;
+import it.ciuverca.agenziaviaggi.repos.UtenteDao;
 import it.ciuverca.agenziaviaggi.repos.ViaggioDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,19 +15,73 @@ import java.util.List;
 @Service
 public class ViaggioServiceImpl implements ViaggioService{
     @Autowired
-    private ViaggioDao dao;
+    private ViaggioDao viaggioDao;
+
+    @Autowired
+    private ClienteDao clienteDao;
+
+    @Autowired
+    private UtenteDao utenteDao;
+
+    @Autowired
+    private PrenotazioniDao prenotazioniDao;
     @Override
     public List<Viaggio> getViaggio() {
-        return dao.findAll();
+        return viaggioDao.findAll();
     }
 
     @Override
     public Viaggio getViaggioById(int id) {
-        return dao.getReferenceById(id);
+        return viaggioDao.getReferenceById(id);
     }
 
     @Override
     public Viaggio addViaggio(Viaggio v) {
-        return dao.save(v);
+        return viaggioDao.save(v);
+    }
+
+    @Override
+    public List<Utente> getUtente() {
+        return utenteDao.findAll();
+    }
+
+    @Override
+    public Utente getUtenteById(int id) {
+        return utenteDao.getReferenceById(id);
+    }
+
+    @Override
+    public Utente addUtente(Utente u) {
+        return utenteDao.save(u);
+    }
+
+    @Override
+    public List<Prenotazioni> getPrenotazioni() {
+        return prenotazioniDao.findAll();
+    }
+
+    @Override
+    public Prenotazioni getPrenotazioniById(int id) {
+        return prenotazioniDao.getReferenceById(id);
+    }
+
+    @Override
+    public Prenotazioni addPrenotazioni(Prenotazioni prenotazioni) {
+        return prenotazioniDao.save(prenotazioni);
+    }
+
+    @Override
+    public List<Cliente> getCliente() {
+        return clienteDao.findAll();
+    }
+
+    @Override
+    public Cliente getClienteById(int id) {
+        return clienteDao.getReferenceById(id);
+    }
+
+    @Override
+    public Cliente addCliente(Cliente cliente) {
+        return clienteDao.save(cliente);
     }
 }
